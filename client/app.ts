d2ab043957497b9d3ab41c027ccddea6b413f781
Nick <Echophase44@github.com>
2023-07-21 16:06:29 -0400

    Created Chore and Person classes and added functionality to the buttons to create each instance.

--- client/app.ts
+++ client/app.ts
@@ -1,1 +1,52 @@
-console.log("Working..")
+const btn_addChore = document.getElementById("btn_addChore")!
+const btn_addPerson = document.getElementById("btn_addPerson")!
+
+
+btn_addChore?.addEventListener("click", () => {
+  event?.preventDefault()
+  new Chore()
+})
+
+btn_addPerson?.addEventListener("click", () => {
+  event?.preventDefault()
+  console.log("test")
+  new PersonCard()
+})
+
+class PersonCard {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById("temp_personCard")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("person_host")! as HTMLDivElement;
+
+    const newPersonNode = document.importNode(this.templateElement.content, true);
+    this.element = newPersonNode.firstElementChild as HTMLDivElement;
+    this.addPerson()
+  }
+
+  private addPerson() {
+    this.hostElement.appendChild(this.element)
+  }
+}
+
+class Chore {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLUListElement;
+  element: HTMLLIElement;
+
+  constructor() {
+    this.templateElement = document.getElementById("temp_chore")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("chores_host")! as HTMLUListElement;
+
+    const newChore = document.importNode(this.templateElement.content, true)
+    this.element = newChore.firstElementChild as HTMLLIElement;
+    this.addChore()
+  }
+
+  private addChore() {
+    this.hostElement.appendChild(this.element)
+  }
+}
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("Working..");
+const btn_addChore = document.getElementById("btn_addChore");
+const btn_addPerson = document.getElementById("btn_addPerson");
+btn_addChore === null || btn_addChore === void 0 ? void 0 : btn_addChore.addEventListener("click", () => {
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    new Chore();
+});
+btn_addPerson === null || btn_addPerson === void 0 ? void 0 : btn_addPerson.addEventListener("click", () => {
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    console.log("test");
+    new PersonCard();
+});
+class PersonCard {
+    constructor() {
+        this.templateElement = document.getElementById("temp_personCard");
+        this.hostElement = document.getElementById("person_host");
+        const newPersonNode = document.importNode(this.templateElement.content, true);
+        this.element = newPersonNode.firstElementChild;
+        this.addPerson();
+    }
+    addPerson() {
+        this.hostElement.appendChild(this.element);
+    }
+}
+class Chore {
+    constructor() {
+        this.templateElement = document.getElementById("temp_chore");
+        this.hostElement = document.getElementById("chores_host");
+        const newChore = document.importNode(this.templateElement.content, true);
+        this.element = newChore.firstElementChild;
+        this.addChore();
+    }
+    addChore() {
+        this.hostElement.appendChild(this.element);
+    }
+}
